07c73513a29ff036541724f2d23dce14e3e52062
Quinten Dobbelaere <quinten.dobbelaere@student.hogent.be>
2022-10-07 10:50:23 +0200

    Working index calc
    area 0-100 with 25cl of Stella for €2,50 as index value 50

--- App.js
+++ App.js
@@ -1,8 +1,53 @@
 import React from "react";
-import { Text } from "react-native";
+import { View } from "react-native";
+import { Text, Surface } from 'react-native-paper';
+import Inputfield from "./app/components/menu/Inputfield";
 
 export default function App() {
+  const [price, setPrice] = React.useState("");
+  const [amountOfCl, setamountOfCl] = React.useState("");
+  const [percentageAlcohol, setpercentageAlcohol] = React.useState("");
+  var color = 'grey';
+
+  function getIndex(){
+    index = (((price/percentageAlcohol)/amountOfCl)*100.00)
+    if(isNaN(index)){
+      return 0
+    }
+    return ((index/1.92)*50).toFixed(0)
+  }
+
+  function perc2color(perc) {
+    var r, g, b = 0;
+    if (perc < 50) {
+      g = 255;
+      r = Math.round((perc/50)*255);
+    }
+    else if(perc < 100) {
+      r = 255;
+      g = Math.round((1-(perc/100))*255);
+    } else {
+      r = 255;
+      g = 0;
+    }
+    var h = r * 0x10000 + g * 0x100 + b * 0x1;
+    return color = ('#' + ('000000' + h.toString(16)).slice(-6));
+}
+
   return (
-    <Text>Hello There</Text>
+    <View style={{flex: 2}}>
+      <View style={{flex: 1, justifyContent:'center', alignContent:'center'}} >
+        <Inputfield name = "Price" onChangeText={setPrice}/>
+        <Inputfield name = "Amount (in cl)" onChangeText={setamountOfCl}/>
+        <Inputfield name = "Percentage of alcohol" onChangeText={setpercentageAlcohol} />
+      </View>
+
+      <View style={{flex: 1, justifyContent:'center', alignContent:'center'}}>
+        <Surface style= {{width: 75, height: 75, justifyContent:'center', alignSelf: 'center', borderRadius: 20, backgroundColor: perc2color(getIndex())}}>
+          <Text style={{alignSelf:'center', fontSize: 32}} >{getIndex()}</Text>
+        </Surface>
+      </View>
+    </View>
   );
+
 }
---
+++ app/components/menu/Inputfield.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { TextInput } from 'react-native-paper';
+
+function checkIfDouble(string, onChangeText){
+  string = string.replace(",",".")
+  if(!isNaN(string)){
+    onChangeText(parseFloat(string));
+  }
+}
+
+export default function Inputfield(props) {
+  return (
+      <TextInput keyboardType='numeric' style={{alignSelf: 'center', margin: 5, width: 300}} label={`${props.name}`} onChangeText={text => checkIfDouble(text, props.onChangeText)}/>
+  );
+}
